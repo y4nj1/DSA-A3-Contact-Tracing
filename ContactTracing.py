a402211de2633de329ef4611d3293a923d161d9d
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-11-12 11:34:15 +0800

    Arranged pseudocode for the program.

---
+++ ContactTracing.py
@@ -0,0 +1,24 @@
+# Write a python program for contact tracing:
+
+# - Display a menu of options
+# 	Menu:
+# 		 1 -> Add an item
+# 		 2 -> Search
+# 		 3 -> Exit (y/n)
+# - Allow user to select item in the menu (check if valid)
+# - Perform the selected option
+# 		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+# 				   Use dictionary to store the info
+# 				   Use the full name as key
+# 				   The value is another dictionary of personal information
+# 		- Option 2: Search, ask full name then display the record
+# 		- Option 3: Ask the user if want to exit or retry.
+
+# =================== CODE PROPER =========================
+# [1] Display Menu
+# [2] Initialize Dictionary: Contacts
+# [3] Ask User Entry
+# [4] Store User Input
+# [5] Recall User Input
+# [6] Print User Input
+# [7] Prompt User: Exit or Retry
